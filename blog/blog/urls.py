494dd965f665b8edd0e8fe501f7c9d4655a7777a
Sanika <sanikaardekar@gmail.com>
2023-01-28 01:43:14 +0530

    protected route for profle added

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('blogApp.urls')),
     path('register/', user_view.register, name="register"),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
-     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout")
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('profile/', user_view.profile, name="profile")
 ]
