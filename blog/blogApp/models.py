cdc5d74ba8465aebe821fbc2d1fdf41dd7d83db7
Sanika <sanikaardekar@gmail.com>
2023-01-28 00:13:48 +0530

    register page done

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
+#sqlite db for dev and postgres for prod
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #date_posted = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(default=timezone.now) #actual function as default value so no ()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted then posts are also deleted not viceversa
+
+    def __str__(self):
+        return self.title
--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import post
 
-posts=[
-    {
-        'author': 'sanika a',
-        'title': 'blog post 1',
-        'content': 'first post content',
-        'date_posted': 'jan 19, 2023'
-    },
-     {
-        'author': 'mudesh a',
-        'title': 'blog post 2',
-        'content': 'second post content',
-        'date_posted': 'jan 20, 2023'
-    },
-]
 # Create your views here.
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': post.objects.all()
     }
     return render(request, 'blogApp/home.html', context)
 
