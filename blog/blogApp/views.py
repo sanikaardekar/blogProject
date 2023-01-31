7c68ff270fe808d5cf43317afc4f2f13d4639d8f
Sanika <sanikaardekar@gmail.com>
2023-02-01 00:14:23 +0530

    list view, detail view, update and delete added cancel button not working on delete

--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -1,13 +1,62 @@
 from django.shortcuts import render
 from .models import post
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
+
 
 def home(request):
     context = {
         'posts': post.objects.all()
     }
     return render(request, 'blogApp/home.html', context)
+
+
+class PostListView(ListView):
+    model = post
+    template_name = 'blogApp/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = post
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if(self.request.user == post.author):
+            return True
+        return False
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if (self.request.user == post.author):
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blogApp/about.html', {'title': 'About'})
