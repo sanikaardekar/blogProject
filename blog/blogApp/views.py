d2c8447d92c9d55fe5783c463b7cfa5be3c0129f
Sanika <sanikaardekar@gmail.com>
2023-02-01 22:38:00 +0530

    pagination and user post page done last page of pagination giving error

--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
@@ -17,6 +18,18 @@ class PostListView(ListView):
     template_name = 'blogApp/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserPostListView(ListView):
+    model = post
+    template_name = 'blogApp/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
