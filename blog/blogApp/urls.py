4b4eededf4c9955b4b3fa98f0e938cd43def1760
Sanika <sanikaardekar@gmail.com>
2023-02-03 17:33:39 +0530

    ui changes

--- blog/blogApp/urls.py
+++ blog/blogApp/urls.py
@@ -4,7 +4,7 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user_posts'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
