d794ba16a0e912b80c6951b4ef9c1ae25f1611fe
Sanika <sanikaardekar@gmail.com>
2023-02-03 17:05:25 +0530

    password reset abbort

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -26,15 +26,6 @@ urlpatterns = [
     path('register/', user_view.register, name="register"),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
-    path('password-reset/',
-    auth_view.PasswordResetView.as_view(template_name='users/password_reset.html'), 
-    name="password_reset"),
-    path('password-reset/done/',
-    auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset.html'), 
-    name="password_reset_done"),
-    path('password-reset-confirm/<uidb64>/<token>',
-    auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
-    name="password_reset_confirm"),
     path('profile/', user_view.profile, name="profile")
 ]
 if settings.DEBUG:
